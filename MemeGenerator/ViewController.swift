1d0971322d4e09865172e32a4c6d37f3a5cbab72
Michael McGuire <Michael.McGuire@gmail.com>
2015-10-01 15:36:32 -0700

    Initial UI

--- MemeGenerator/ViewController.swift
+++ MemeGenerator/ViewController.swift
@@ -2,5 +2,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var topTextField: UITextField!
+  @IBOutlet weak var bottomTextField: UITextField!
+  @IBOutlet weak var generateButton: UIButton!
+  @IBOutlet weak var memeImageView: UIImageView!
+  @IBOutlet weak var networkActivityIndicatorView: UIActivityIndicatorView!
+  
 }
 
