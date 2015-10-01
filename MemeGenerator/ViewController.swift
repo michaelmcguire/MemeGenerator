f5f027c6c0e9562a62c9f34e86219c9b59194b2c
Michael McGuire <Michael.McGuire@gmail.com>
2015-10-01 12:09:44 -0700

    Cleanup: remove unnecessary methods and comments.  Allow Xcode to do its thing to the storyboard file

--- MemeGenerator/ViewController.swift
+++ MemeGenerator/ViewController.swift
@@ -1,25 +1,6 @@
-//
-//  ViewController.swift
-//  MemeGenerator
-//
-//  Created by Michael C McGuire on 10/1/15.
-//
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
 
 }
 
