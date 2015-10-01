efc42f354987505864d982fafa0fc13f8d50aa6f
Michael McGuire <Michael.McGuire@gmail.com>
2015-10-01 16:58:54 -0700

    Adding Cancel button

--- MemeGenerator/ViewController.swift
+++ MemeGenerator/ViewController.swift
@@ -5,6 +5,7 @@ class ViewController: UIViewController {
   @IBOutlet weak var topTextField: UITextField!
   @IBOutlet weak var bottomTextField: UITextField!
   @IBOutlet weak var generateButton: UIButton!
+  @IBOutlet weak var cancelButton: UIButton!
   @IBOutlet weak var memeImageView: UIImageView!
   @IBOutlet weak var networkActivityIndicatorView: UIActivityIndicatorView!
 
