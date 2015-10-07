a983888bbed7a1a748ce1baf0d79d0ee18fc4bfb
Michael McGuire <Michael.McGuire@gmail.com>
2015-10-07 15:11:59 -0700

    Adding enabling/disabling buttons based on request executing.  Adding cancelation of request.

--- MemeGenerator/ViewController.swift
+++ MemeGenerator/ViewController.swift
@@ -1,5 +1,6 @@
 import UIKit
 import ReactiveCocoa
+import Rex
 
 class ViewController: UIViewController {
 
@@ -11,17 +12,30 @@ class ViewController: UIViewController {
   @IBOutlet weak var networkActivityIndicatorView: UIActivityIndicatorView!
   
   let server = Server()
+  var lastRequest: Disposable?
+  
+  lazy var request: Action<(String, String), UIImage, NSError> = Action { [unowned self] (top, bottom) in
+    return self.server.generateMeme(top, bottom: bottom).observeOn(UIScheduler())
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  
+    generateButton.rex_enabled <~ request.executing.producer.map { !$0 }
+    cancelButton.rex_enabled <~ request.executing
+  }
   
   @IBAction func generateTapped(sender: UIButton) {
-    
-    server.generateMeme(topTextField.text!, bottom: bottomTextField.text!)
-      .observeOn(UIScheduler())
+    lastRequest = self.request.apply((topTextField.text!, bottom: bottomTextField.text!))
       .startWithNext { image in
         self.memeImageView.image = image
       }
-    
   }
   
+  @IBAction func cancelTapped(sender: UIButton) {
+    lastRequest?.dispose()
+    lastRequest = nil
+  }
   
 }
 
